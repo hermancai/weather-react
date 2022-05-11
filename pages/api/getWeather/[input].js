06dbfc6bfee9cb295fe89240b0bf9b23f2efaca1
hermancai <30858461+hermancai@users.noreply.github.com>
2022-05-10 23:11:36 -0700

    Write route to get data from api

---
+++ pages/api/getWeather/[input].js
@@ -0,0 +1,17 @@
+export default async function getWeather({ query: { input } }, res) {
+  const coordsResponse = await fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=1&appid=${process.env.WEATHER_API}`
+  );
+  const coordsJSON = await coordsResponse.json();
+
+  if (coordsJSON.length === 0)
+    res.status(400).json({ error: "No results found." });
+
+  const { lat, lon } = coordsJSON[0];
+  const forecastResponse = await fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,minutely,alerts&units=imperial&appid=${process.env.WEATHER_API}`
+  );
+  const forecastJSON = await forecastResponse.json();
+
+  res.status(200).json(forecastJSON);
+}
